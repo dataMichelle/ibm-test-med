cf70ecc14cabc1e77dc5b65638a1200e31d7e5e2
dataMichelle <data_Michelle@proton.me>
2025-05-21 13:28:15 -0400

    fixed navbar styling

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   window.location.hostname === "localhost"
-    ? "<add your theia server side url>"
+    ? "https://michellebsal-5500.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/test_med/src/server/"
     : "add your theia server side url";
 console.log("API_URL :", API_URL);
