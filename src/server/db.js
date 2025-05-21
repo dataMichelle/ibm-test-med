0447c1cd16355e5bb707f127cd4a363969af6904
dataMichelle <data_Michelle@proton.me>
2025-05-21 11:45:29 -0400

    first commit

---
+++ src/server/db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// Set your MongoDB URI here
+const mongoURI = "mongosh mongodb://root:0AKaQWm6F4QzYhP1zh8b13rb@172.21.177.41:27017"
+
+const connectToMongo = async (retryCount) => {
+  const MAX_RETRIES = 3;
+  const count = retryCount ?? 0;
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    if (count < MAX_RETRIES) {
+      console.log(
+        `Retrying MongoDB connection (${count + 1}/${MAX_RETRIES})...`
+      );
+      await connectToMongo(count + 1);
+    } else {
+      console.error("Failed to connect to MongoDB:", err);
+      process.exit(1);
+    }
+  }
+};
+
+module.exports = connectToMongo;
